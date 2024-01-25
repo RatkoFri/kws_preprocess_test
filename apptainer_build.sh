39816a00125018528032c7172785d202e4389cc1
RatkoFri <RatkoFri>
2024-01-25 14:10:50 +0100

    Added apptainer scripts.

---
+++ apptainer_build.sh
@@ -0,0 +1,3 @@
+export APPTAINER_DISABLE_CACHE=true
+
+apptainer build --fakeroot kws_apptainer.sif kws_apptainer.def 
---
+++ apptainer_run.sh
@@ -0,0 +1,1 @@
+singularity exec --nv kws_apptainer.sif python3 ../kws.py | tee $PWD/log/kws_asses.log
